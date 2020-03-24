1d7f753f48ca77d7f4730066e822593e4e07f746
nvictoria987 <32154280+nvictoria987@users.noreply.github.com>
2020-03-23 19:14:18 -0700

    database files

---
+++ takeahikevictoria/takeahikevictoria/firstDB.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <vector>
+#include <memory>     // make_unique()
+
+#include "firstDB.hpp"
+//#include "TechnicalServices/Logging/SimpleLogger.hpp"
+#include "PersistenceHandler.hpp"
+
+
+namespace Persistence
+{
+	SimpleDB::SimpleDB()
+		//: _loggerPtr(std::make_unique<TechnicalServices::Logging::SimpleLogger>())     // will replace these factory calls with abstract factory calls next increment
+	{
+		//_logger << "Simple DB being used and has been successfully initialized";
+	}
+
+
+
+	SimpleDB::~SimpleDB() noexcept
+	{
+		//_logger << "Simple DB shutdown successfully";
+	}
+
+
+
+	std::vector<std::string> SimpleDB::findRoles()
+	{
+		return { "TrailAdmin", "TrailUser"};
+	}
+
+	UserCredentials SimpleDB::findCredentialsByName(const std::string & name)
+	{
+		static std::vector<UserCredentials> storedUsers =
+		{
+			// Username    Pass Phrase         Authorized roles
+			  {"victoria",     "please work",  {"TrailUser","TrailAdmin"}},
+			  {"eric", "password123",   {"TrailAdmim" }},
+			  {"chase",  "123",                 {"TrailUser" }}
+		};
+
+		for (const auto & user : storedUsers) if (user.userName == name) return { user.userName, user.passPhrase, user.roles };
+
+		// Name not found, throw something
+		std::string message = __func__;
+		message += " attempt to find user \"" + name + "\" failed";
+
+	//	_logger << message;
+
+		throw PersistenceHandler::NoSuchUser(message);
+	}
+
+} // namespace TechnicalServices::Persistence
---
+++ takeahikevictoria/takeahikevictoria/firstDB.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <memory>    // unique_ptr
+#include <string>
+#include <vector>
+
+//#include "TechnicalServices/Logging/LoggerHandler.hpp"
+#include "PersistenceHandler.hpp"
+
+
+
+
+namespace Persistence
+{
+  class SimpleDB : public Persistence::PersistenceHandler
+  {
+    public:
+      using PersistenceHandler::PersistenceHandler;    // inherit constructors
+      SimpleDB();
+
+
+      // Operations
+      std::vector<std::string> findRoles()                                       override;  // Returns list of all legal roles
+      UserCredentials          findCredentialsByName( const std::string & name ) override;  // Returns credentials for specified user, throws NoSuchUser if user not found
+
+
+      ~SimpleDB() noexcept override;
+
+    private:
+      //std::unique_ptr<TechnicalServices::Logging::LoggerHandler>             _loggerPtr;
+
+      // convenience reference object enabling standard insertion syntax
+      // This line must be physically after the definition of _loggerPtr
+     // TechnicalServices::Logging::LoggerHandler                            & _logger = *_loggerPtr;
+
+  }; // class SimpleDB
+}  // namespace TechnicalServices::Persistence
+
